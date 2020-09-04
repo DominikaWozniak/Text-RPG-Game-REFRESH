feaead6526b8ab8f4a2fbab1b10f0018ca606659
domin <dominika.gothub@gmail.com>
2020-09-04 13:39:18 +0200

    refilled Enemy.class, Loot.class, changed armor.getAdditionalArmorPoints() to armor.getArmorPoints() in ArmorServiceImpl.class

--- src/main/java/services/impl/ArmorServiceImpl.java
+++ src/main/java/services/impl/ArmorServiceImpl.java
@@ -8,7 +8,7 @@ public class ArmorServiceImpl implements ArmorService {
     @Override
     public void screenArmorItems(Armor armor) {
         System.out.println("Typ zbroi: " + armor.getArmorType()+"\n" +
-                "Punkty do zbroi: " + armor.getAdditionalArmorPoints() + "\n" +
+                "Punkty do zbroi: " + armor.getArmorPoints() + "\n" +
                 "Charyzma: " + armor.getCharisma() + "\n" +
                 "Siła: " + armor.getStrength() + "\n" +
                 "Zwinność: " + armor.getAgility() + "\n" +
